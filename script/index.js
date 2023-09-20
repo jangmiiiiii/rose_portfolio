cc3ded90a573bc685648847330d74a5c5c3e303e
jangmi <wkdal9748@naver.com>
2023-09-20 17:38:30 +0900

    design slide

---
+++ script/index.js
@@ -0,0 +1,11 @@
+// detail design slide 시작
+var mySwiper = new Swiper('#detail_slide ', {
+    slidesPerView:2,
+    spaceBetween:30,
+    //페이지 이전/다음 버튼
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+// detail design slide 끝
